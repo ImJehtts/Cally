4587b5d88e82b1c0b5442d833292c6ef3607331b
Mohib Rehman <rehmanmohib08@gmail.com>
2026-01-13 00:06:00 -0500

    Created Dropdown menu
    Created the dropdown menu to select the current and next 3 (4 total) months.
    The component does not load correctly on launch as it is higher up.

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Dropdown } from 'react-native-element-dropdown';
+import React from 'react';
+import { useState } from 'react';
+
 
 export default function App() {
+  const currentMonths = [
+    {label: 'January 2026', value: '01'},
+    {label: 'February 2026', value: '02'},
+    {label: 'March 2026', value: '03'},
+    {label: 'April 2026', value: '04'},
+  ];
+  
+  const [currentmonthValue, setcurrentmonthValue] = React.useState(currentMonths[0].value);
+
   const profilePicturePressed = async () => {
     alert('Profile Picture pressed!');
   };
+
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -14,6 +29,17 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <View style={styles.monthSelector}>
+        <Dropdown
+          style={styles.monthDropdown}
+          data={currentMonths}
+          labelField="label"
+          valueField="value"
+          value={currentmonthValue}
+          onChange={item => {
+            setcurrentmonthValue(item.value)}}
+          selectedTextStyle={styles.selectedTextStyle}
+          renderItem={item => <Text style={styles.itemText}>{item.label}</Text>}
+        />
       </View>
     </SafeAreaView>
   );
@@ -30,11 +56,11 @@ const styles = StyleSheet.create({
     paddingVertical: 16,
     alignItems: 'center',
     justifyContent: 'center',
-    position: 'relative',
   },
   headerText: {
     fontSize: 32,
     fontWeight: '500',  
+    fontFamily: 'DMSerifDisplay-Regular-Italic',
     color: '#000000',
   },
   profilePicture: {
@@ -48,5 +74,25 @@ const styles = StyleSheet.create({
     borderWidth: 2.5, 
   },
   monthSelector: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  monthDropdown: {
+    height: 50,
+    width: '65%',
+    borderColor: '#7A5C3E',
+    borderWidth: 2,
+    borderRadius: 25,
+    paddingHorizontal: 12,
+    backgroundColor: '#FFFFFF',
+  },
+  selectedTextStyle: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  itemText: {
+    padding: 7,
+    fontSize: 18,
+    textAlign : 'center',
   },
 });
