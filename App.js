385f82dac13860687b6dadcd3147922fc1ed9671
Mohib Rehman <rehmanmohib08@gmail.com>
2026-01-12 23:17:29 -0500

    Add safeareaview and Add Orario header with section for profile picture
    Still need to apply correct text style.

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const profilePicturePressed = async () => {
+    alert('Profile Picture pressed!');
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Orario</Text>
+        <TouchableOpacity style={styles.profilePicture} 
+          onPress={profilePicturePressed}>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.monthSelector}>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FBF7F2',
+    paddingHorizontal: 20,
+  },
+  header: {
+    marginTop: 20,
+    paddingVertical: 16,
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative',
+  },
+  headerText: {
+    fontSize: 32,
+    fontWeight: '500',  
+    color: '#000000',
+  },
+  profilePicture: {
+    position: 'absolute',
+    right: 0,
+    width: 40,
+    height: 40,
+    backgroundColor: '#D3D3D3',
+    borderRadius: 20,
+    borderColor: '#7A5C3E',
+    borderWidth: 2.5, 
+  },
+  monthSelector: {
   },
 });
